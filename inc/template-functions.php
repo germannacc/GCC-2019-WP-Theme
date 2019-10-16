d2590157e3c3846a88da02cee9472f91e90b6b52
Matt Hummel <marketing@users.noreply.github.com>
2019-10-16 09:38:00 -0400

    Updated
    Added function to display wp list pages in sidebar menu.

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -48,10 +48,104 @@ if ( ! function_exists( 'gcc_wp_2018_posted_on' ) ) :
 endif;
 
 /**
-* Removes or edits the 'Protected:' part from posts titles
+* Removes or edits the 'Protected:' part from posts titles protected with a password.
 */
 
 add_filter( 'protected_title_format', 'remove_protected_text' );
 function remove_protected_text() {
 return __('%s');
 }
+/** Function for displaying only child pages of parent. **/
+function gcc_wp_2018_list_child_pages( $args = '' ) { 
+
+global $post;
+$top_parent = $post->ID;
+$defaults = array(    
+        'title_li' => '',
+        'child_of' => $top_parent,
+        'exclude'      => '',
+        'title_li'     => __( 'In This Section' ),
+        'echo'         => 1,
+        'authors'      => '',
+        'sort_column'  => 'menu_order, post_title',
+        'sort_order' => 'asc',
+        'link_before'  => '',
+        'link_after'   => '',
+        'item_spacing' => 'preserve',
+        'walker'       => '',
+        'sort_order' => 'asc',
+        'sort_column' => 'post_title',
+        'depth' => 3
+    );
+ 
+    $r = wp_parse_args( $args, $defaults );
+ 
+    if ( ! in_array( $r['item_spacing'], array( 'preserve', 'discard' ), true ) ) {
+        // invalid value, fall back to default.
+        $r['item_spacing'] = $defaults['item_spacing'];
+    }
+ 
+    $output       = '';
+    $current_page = 0;
+ 
+    // sanitize, mostly to keep spaces out
+    $r['exclude'] = preg_replace( '/[^0-9,]/', '', $r['exclude'] );
+ 
+    // Allow plugins to filter an array of excluded pages (but don't put a nullstring into the array)
+    $exclude_array = ( $r['exclude'] ) ? explode( ',', $r['exclude'] ) : array();
+ 
+    /**
+     * Filters the array of pages to exclude from the pages list.
+     *
+     * @since 2.1.0
+     *
+     * @param array $exclude_array An array of page IDs to exclude.
+     */
+    $r['exclude'] = implode( ',', apply_filters( 'wp_list_pages_excludes', $exclude_array ) );
+ 
+    // Query pages.
+    $r['hierarchical'] = 0;
+    $pages             = get_pages( $r );
+ 
+    if ( ! empty( $pages ) ) {
+        if ( $r['title_li'] ) {
+            $output .= '<div class="widget"><h3>' . $r['title_li'] . '</h3><ul class="vertical menu accordion-menu"  data-accordion-menu data-submenu-toggle>';
+        }
+        global $wp_query;
+        if ( is_page() || is_attachment() || $wp_query->is_posts_page ) {
+            $current_page = get_queried_object_id();
+        } elseif ( is_singular() ) {
+            $queried_object = get_queried_object();
+            if ( is_post_type_hierarchical( $queried_object->post_type ) ) {
+                $current_page = $queried_object->ID;
+            }
+        }
+ 
+        $output .= walk_page_tree( $pages, $r['depth'], $current_page, $r );
+ 
+        if ( $r['title_li'] ) {
+            $output .= '</ul></div>';
+        }
+    }
+ 
+    /**
+     * Filters the HTML output of the pages to list.
+     *
+     * @since 1.5.1
+     * @since 4.4.0 `$pages` added as arguments.
+     *
+     * @see wp_list_pages()
+     *
+     * @param string $output HTML output of the pages list.
+     * @param array  $r      An array of page-listing arguments.
+     * @param array  $pages  List of WP_Post objects returned by `get_pages()`
+     */
+    $html = apply_filters( 'wp_list_pages', $output, $r, $pages );
+ 
+    if ( $r['echo'] ) {
+        echo $html;
+    } else {
+        return $html;
+    }
+}
+add_shortcode('gcc_wp_2018_childpages', 'gcc_wp_2018_list_child_pages');
