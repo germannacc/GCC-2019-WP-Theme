badbb058313d816b7ff8e4526f135055063e3976
Matt Hummel <marketing@users.noreply.github.com>
2019-10-16 09:32:18 -0400

    Update
    Added ajax loading to post with Ajax load more plugin.

--- single.php
+++ single.php
@@ -10,12 +10,50 @@ get_header(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php
 	while ( have_posts() ) : the_post(); ?>
-	<?php //Single Post Page Heading
+
+<!--Page Content-->
+<div class="row gutter-small expanded content-area">
+	<div class="columns small-12 medium-9">
+		<div class="entry-content" id="main" tabindex="0">
+
+<?php //Single Post Page Heading
 	get_template_part( 'template-parts/content', 'single-heading' );
-	?>
-	<?php //Single Post Content
-	get_template_part( 'template-parts/content', 'single' );
-	?>
+?>
+
+			<?php
+			the_content( sprintf(
+			wp_kses(
+			/* translators: %s: Name of current post. Only visible to screen readers */
+			__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'gcc-wp-2018' ),
+			array(
+				'span' => array(
+					'class' => array(),
+				),
+			)
+			),
+			get_the_title()
+			)
+			); ?>
+			<?php getPrevNext();
+			get_the_category_list();
+			?>
+			<?php
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+			?>
+		</div>
+	</div>
+	<?php //Template Sidebar
+	get_sidebar(); ?>
+	</div><!--.pagecontent-->
+	<?php if ( get_edit_post_link() ) : ?>
+	<footer class="entry-footer">
+		<?php gcc_wp_2018_entry_footer(); ?>
+		</footer><!-- .entry-footer -->
+		<?php endif; ?>
+
 	<?php endwhile; // End of the loop. ?>
 </article>
 <?php
--- template-parts/content-highlights.php
+++ template-parts/content-highlights.php
@@ -2,14 +2,21 @@
 //highlights section
 $highlights_heading = get_field('highlights_heading');
 ?>
+
 <div id="highlights" class="row expanded" data-equalizer>
 	<div class="highlights-content">
 		<h2  class="text-center"><?php the_field('highlights_heading'); ?></h2>
+		
 		<div id="4080701046" class="large-8 columns collapse">
-			<?php get_template_part( 'template-parts/content', 'latest-post' );
+			
+	   <?php get_template_part( 'template-parts/content', 'latest-post' );
 			?>
-			<a href="<?php esc_html_e('/blog/', 'gcc-wp-2018' ); ?>" class="button expanded"><?php esc_html_e('View all highlights', 'gcc-wp-2018' ); ?></a>
+			
+		<?php echo do_shortcode( '
+			[ajax_load_more post_type="post" posts_per_page="1" pause="true" scroll="false" button_label="Show More"]' ); ?>
+
 		</div>
+
 		<div class="large-4 columns collapse">
 			<?php get_template_part( 'template-parts/content', 'trending' );
 			?>
