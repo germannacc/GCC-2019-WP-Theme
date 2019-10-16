b1dde5e79d6ec96c4eceab3ef335c3a1a5f3dd88
Matt Hummel <marketing@users.noreply.github.com>
2019-10-16 09:34:46 -0400

    Update
    Removed next previous links for ajax load more.

--- template-parts/content-postpage.php
+++ template-parts/content-postpage.php
@@ -2,11 +2,10 @@
 <div class="row gutter-small expanded content-area">
   <div class="small-12 medium-9 entry-content" id="main" tabindex="0">
     <?php
-    $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+
     $args =  array (
     'post_type' => 'post',
-    'posts_per_page'=>-5,
-    'paged'          => $paged
+    'posts_per_page'=>5
     );
     ?>
     <?php
@@ -53,15 +52,16 @@
         ); ?></p>
       </div>
     </div>
+
+  
     <?php  endif; ?>
     <?php endwhile;  ?>
     <?php wp_reset_postdata(); ?>
     <?php else : ?>
     <p><?php esc_html_e( 'Sorry, no posts matched your criteria.', 'gcc-wp-2018' ); ?></p>
     <?php endif; ?>
-    <div class="nav-previous alignleft"><?php next_posts_link( '<span class="fa fa-chevron-left" aria-hidden="true" style="padding-right: .5rem;"></span>Older posts' ); ?></div>
-    <div class="nav-next alignright"><?php previous_posts_link( 'Newer posts <span class="fa fa-chevron-right" aria-hidden="true" style="padding-left: .5rem;"></span>' ); ?>
-  </div>
+    
+
 </div>
 <?php //Template Sidebar
 get_sidebar(); ?>
